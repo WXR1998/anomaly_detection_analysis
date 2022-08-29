55271871b3126480bbdf11b51c677127ad99bb08
Xuanrun Wang <810158407@qq.com>
2022-08-29 15:40:15 +0800

    新的通信协议实现

--- model/time_series.py
+++ model/time_series.py
@@ -1,34 +1,58 @@
-from typing import Optional, Union
+from typing import Optional, Union, Any, Iterable
+
+import math
 import numpy as np
 import datetime
+
+from sam.base.p4NFUsage import P4NFUsage
+
 
 class TimeSeries:
     def __init__(self,
                  value: Union[list, tuple, np.ndarray]=tuple(),
+                 timestamp: Union[list, tuple, np.ndarray]=tuple(),
                  normal_window_length: int=60):
         """
         对于时间序列的前normal_window_length个元素，认为其是正常的。
         用正常的部分训练模型、获取算法需要的超参数。
         """
 
-        value = np.array(value, dtype=float)
-
-        self._value = value
-        self._timestamp = np.array([], dtype=int)
+        assert len(value) == len(timestamp)
+        self._value = list(value)
+        self._timestamp = list(timestamp)
         self._normal_window_length = normal_window_length
+        self._mu = 0
+        self._sigma = 1
 
-    def add(self, value: float):
+    def add(self, value: Any, timestamp: int=0):
         if value is None:
             value = 0
-        self._value = np.append(self._value, value)
-        self._timestamp = np.append(self._timestamp, int(datetime.datetime.now().timestamp()))
+        if isinstance(value, Iterable):
+            value = float(np.mean(value))
+        if isinstance(value, P4NFUsage):
+            value = 0
 
-    def value(self, normal_part: bool=False) -> np.ndarray:
+        n = len(self._value)
+        if len(self._value) <= self._normal_window_length:
+            mu = (self._mu * n + value) / (n + 1)
+            sigma = math.sqrt((n * (self._sigma**2 + (mu - self._mu)**2) + (mu - self._mu)**2) / (n + 1))
+            self._mu = mu
+            self._sigma = sigma
+
+        self._value.append(value)
+        self._timestamp.append(timestamp)
+
+    def tail_value(self) -> Optional[float]:
+        if len(self._value) == 0:
+            return None
+        return self._value[-1]
+
+    def value(self, normal_part: bool=False) -> list:
         if normal_part and len(self._value) > self._normal_window_length:
             return self._value[:self._normal_window_length]
         return self._value
 
-    def timestamp(self, normal_part: bool=False) -> np.ndarray:
+    def timestamp(self, normal_part: bool=False) -> list:
         if normal_part and len(self._timestamp) > self._normal_window_length:
             return self._timestamp[:self._normal_window_length]
         return self._timestamp
